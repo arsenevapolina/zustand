5b6e3e766a392e78fc9bfb98a479ea004fd93b0a
Полина <pechenkova.polina.mgu@yandex.ru>
2025-03-15 23:29:50 +0300

    feature - created to do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import { Card, Checkbox, Input } from "antd";
 import "./App.css";
-import { addTen } from "./helpers/addTen";
-import { useCounterStore } from "./modal/counterStore";
+import { useTodoStore } from "./modal/todoStore";
+import { useState } from "react";
 
 function App() {
-  const { counter, decrement, increment } = useCounterStore();
+  const { addTodo, changeIsComplete, todos } = useTodoStore();
+  const [value, setValue] = useState<string>("");
 
   return (
     <div className="wrapper">
-      <button onClick={increment}>+</button>
-      <span>{counter}</span>
-      <button onClick={decrement}>-</button>
-      <button onClick={addTen}>add 10</button>
+      <Input
+        style={{ width: 300 }}
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addTodo(value);
+            setValue("");
+          }
+        }}
+      />
+      {todos.map((todo, index) => (
+        <Card className="card">
+          <Checkbox
+            checked={todo.isComplete}
+            onChange={() => changeIsComplete(index)}
+          />
+          <span>{todo.title}</span>
+        </Card>
+      ))}
     </div>
   );
 }
