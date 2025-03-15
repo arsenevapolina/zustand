cf40f4a46d787e27720d16dfa4be592576002f11
Полина <pechenkova.polina.mgu@yandex.ru>
2025-03-15 20:30:42 +0300

    feature - getting data from State

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import { useCounterStore } from "./modal/counterStore";
 
 function App() {
+  const { counter } = useCounterStore();
 
   return (
-    <>
-    </>
-  )
+    <div className="wrapper">
+      <span>{counter}</span>
+    </div>
+  );
 }
 
-export default App
+export default App;
